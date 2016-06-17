fe4134d56f9c862dbcbea11e054f94e258f05646
MikeTeok <miketeok94@gmail.com>
2016-06-18 00:01:55 +0800

    Update Program.cs

--- AutoAbilityLevelUp/Program.cs
+++ AutoAbilityLevelUp/Program.cs
@@ -37,9 +37,9 @@ namespace AutoAbilityLevelUp
 
         #endregion
     }
-  
-        #endregion
-    
+
+    #endregion
+
     internal class Program
     {
         #region member
@@ -51,16 +51,16 @@ namespace AutoAbilityLevelUp
         private static int[] sequence = new int[25];
         private static bool loaded = false, skilltrue = false, hold = false, save = false, delete = false, start = false, startcache = false;
         private static bool[,] skill2d = new bool[5, 25], skill2dcasche = new bool[5, 25];
-        private static bool _leftMouseIsPress, leftMouseIsHold, move;
+        private static bool _leftMouseIsPress, leftMouseIsHold;
         private static bool[] offskill1 = new bool[25];
         private static readonly Menu Menu = new Menu("AutoAbilityLevelUp", "rootmenu", true);
-        private static Vector2 startloc = new Vector2(450, 110);
+        private static Vector2 startloc = new Vector2(450, 110), diff;
         private static readonly Dictionary<string, DotaTexture> TextureDictionary = new Dictionary<string, DotaTexture>();
         private static readonly string MyPath = System.IO.Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName) + "\\AutoAbilityLevelUp\\";
         private static string[] name = new string[5];
         #endregion
 
-            
+
         private static void Main()
         {
             Menu.AddItem(new MenuItem("table", "Show Table").SetValue(new KeyBind('K', KeyBindType.Toggle)));
@@ -117,7 +117,7 @@ namespace AutoAbilityLevelUp
             if (!skilltrue)
             {
                 spell[0] = me.Spellbook.SpellQ;
-                if(ObjectManager.LocalHero.ClassID == ClassID.CDOTA_Unit_Hero_Nevermore) spell[1] = me.Spellbook.SpellD;
+                if (ObjectManager.LocalHero.ClassID == ClassID.CDOTA_Unit_Hero_Nevermore) spell[1] = me.Spellbook.SpellD;
                 else spell[1] = me.Spellbook.SpellW;
                 if (ObjectManager.LocalHero.ClassID == ClassID.CDOTA_Unit_Hero_Chen || ObjectManager.LocalHero.ClassID == ClassID.CDOTA_Unit_Hero_Beastmaster) spell[2] = me.Spellbook.SpellD;
                 else if (ObjectManager.LocalHero.ClassID == ClassID.CDOTA_Unit_Hero_Nevermore) spell[2] = me.Spellbook.SpellF;
@@ -156,6 +156,7 @@ namespace AutoAbilityLevelUp
                     }
                 }
                 for (int i = 0; i < skillnum; i++) name[i] = NameManager.Name(spell[i]);
+                TextureDictionary.Add("itembg1", Drawing.GetTexture("materials/ensage_ui/menu/itembg1.vmat"));
                 skilltrue = true;
             }
             if (save)
@@ -233,14 +234,11 @@ namespace AutoAbilityLevelUp
                     }
                 }
             }
-            if (move)
-            {
-                startloc = Game.MouseScreenPosition + new Vector2(0, 6);
-            }
             if (Menu.Item("table").GetValue<KeyBind>().Active)
             {
                 DotaTexture texture;
-
+                if (TextureDictionary.TryGetValue("itembg1", out texture))
+                    Drawing.DrawRect(startloc - new Vector2(25,12), new Vector2(725, 137), texture);
                 for (int i = 0; i < skillnum; i++)
                 {
 
@@ -259,10 +257,10 @@ namespace AutoAbilityLevelUp
                         DrawButton(startloc + new Vector2((i * 25), (j * 25)), 25, 25, ref skill2d[j, i], offskill1[i], new Color(0, 255, 0, 25), new Color(0, 0, 0, 50), "" + (i + 1));
                     }
                 }
-                DrawButton(startloc + new Vector2(((18+skillnum) * 25), ((skillnum) * 25)), 50, 25, ref save, true, new Color(0, 255, 0, 25), new Color(0, 0, 0, 50), "SAVE");
-                DrawButton(startloc + new Vector2(((16 + skillnum) * 25), ((skillnum) * 25)), 50, 25, ref delete, true, new Color(0, 255, 0, 25), new Color(0, 0, 0, 50), "DELETE");
-                DrawButton(startloc + new Vector2(((20 + skillnum) * 25), 0), 75, 25*skillnum, ref start, true, new Color(0, 255, 0, 25), new Color(0, 0, 0, 50), "START");
-                DragButton(startloc - new Vector2(25, 12), 50, 12, ref move);
+                DrawButton(startloc + new Vector2(((20 + skillnum) * 25), ((skillnum-1) * 25)), 75, 25, ref save, true, new Color(0, 255, 0, 25), new Color(0, 0, 0, 50), "SAVE");
+                DrawButton(startloc + new Vector2(((20 + skillnum) * 25), ((skillnum-2) * 25)), 75, 25, ref delete, true, new Color(0, 255, 0, 25), new Color(0, 0, 0, 50), "DELETE");
+                DrawButton(startloc + new Vector2(((20 + skillnum) * 25), 0), 75, 25 * (skillnum-2), ref start, true, new Color(0, 255, 0, 25), new Color(0, 0, 0, 50), "START");
+                DragButton(startloc - new Vector2(25, 12), 725, 12);
             }
         }
         private static void updatearray(int j, int i)
@@ -339,7 +337,7 @@ namespace AutoAbilityLevelUp
             var isIn = Utils.IsUnderRectangle(Game.MouseScreenPosition, a.X, a.Y, w, h);
             if (isActive)
             {
-                if (_leftMouseIsPress && Utils.SleepCheck("ClickButtonCd") && isIn)
+                if (_leftMouseIsPress && Utils.SleepCheck("ClickButtonCd") && isIn && !hold)
                 {
                     clicked = !clicked;
                     Utils.Sleep(250, "ClickButtonCd");
@@ -348,7 +346,7 @@ namespace AutoAbilityLevelUp
                     ? new Color((int)(clicked ? @on.R : off.R), clicked ? @on.G : off.G, clicked ? @on.B : off.B, 150)
                     : clicked ? @on : off;
                 Drawing.DrawRect(a, new Vector2(w, h), newColor);
-                Drawing.DrawRect(a, new Vector2(w, h), new Color(0, 0, 0, 255), true);
+                Drawing.DrawRect(a, new Vector2(w, h), new Color(255, 255, 255, 10), true);
                 if (drawOnButtonText != "")
                 {
                     Drawing.DrawText(drawOnButtonText, a + new Vector2(10, 2), Color.White,
@@ -367,21 +365,22 @@ namespace AutoAbilityLevelUp
                 }
             }
         }
-        private static void DragButton(Vector2 a, float w, float h, ref bool clicked)
+        private static void DragButton(Vector2 loc, float w, float h)
         {
-            var isIn = Utils.IsUnderRectangle(Game.MouseScreenPosition, a.X, a.Y, w, h);
-            if (leftMouseIsHold && Utils.SleepCheck("HoldButtonCd") && isIn)
+            var isIn = Utils.IsUnderRectangle(Game.MouseScreenPosition, loc.X, loc.Y, w, h);
+
+            if (leftMouseIsHold && Utils.SleepCheck("HoldButtonCd") && isIn && !hold)
             {
                 hold = true;
+                diff = Game.MouseScreenPosition - startloc;
                 Utils.Sleep(250, "HoldButtonCd");
             }
             else if (!leftMouseIsHold) hold = false;
-            if (hold) clicked = true;
-            else clicked = false;
-            var newColor = isIn
-                ? new Color(255, 255, 255, 50) : clicked ? new Color(0, 0, 0, 100) : new Color(0, 0, 0, 50);
-            Drawing.DrawRect(a, new Vector2(w, h), newColor);
-            Drawing.DrawRect(a, new Vector2(w, h), new Color(0, 0, 0, 255), true);
+            if (hold)
+            {
+                startloc = Game.MouseScreenPosition - diff;
+            }
+            Drawing.DrawRect(loc, new Vector2(w, h), new Color(0, 0, 0, 150));
         }
     }
 }
